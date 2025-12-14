e347d8288742ea6f7eb73d9ff74e10501d76dd4c
Anfioo <3485977506@qq.com>
2025-12-14 18:11:19 +0800

    空模板,改为其他的模板

--- src/data/todo.ts
+++
@@ -1,9 +0,0 @@
-import type { TodoConfig } from "@/types/config";
-
-export const todoConfig: TodoConfig = {
-    enable: true,
-    title: "待办事项",
-    items: [
-        { content: "为博客添加”待办事项“功能", completed: true }
-    ],
-};
